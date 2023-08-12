f8366d5e4272d72b5e9e8efe552a9f181a2d2944
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-12 21:44:57 +0200

    changes

--- customJsConfirmationPage.js
+++ customJsConfirmationPage.js
@@ -1,90 +1,115 @@
-// Huidige URL ophalen
-const url = window.location.href;
+// Wordt geactiveerd wanneer de pagina is geladen
+window.addEventListener('load', function () {
 
-// Array met vereiste parameters
-const requiredParameters = ["taskid", "formid", "opdref", "opdrachtgever", "opdrachtgeverid", "naamklant", "adres", "soortklus", "formname", "dlform", "dlsign"];
-
-// Functie om te controleren of een parameter bestaat in de URL
-function controleerParameter(parameterNaam) {
-    const urlParameters = new URLSearchParams(url);
-    return urlParameters.has(parameterNaam);
-}
-
-// Functie om een URL-parameter te extraheren
-function extractUrlParameter(key) {
-    const urlObj = new URL(url);
-    const params = new URLSearchParams(urlObj.search);
-    return params.get(key);
-}
-
-// Start codeverwerking functie
-function runMainJs() {
-
-    // Declare url
+    // Huidige URL ophalen
     const url = window.location.href;
 
-    // Extract parameters using the extractUrlParameter function
-    const taskid = extractUrlParameter("taskid");
-    const formid = extractUrlParameter("formid");
-    const opdref = extractUrlParameter("opdref");
-    const opdrachtgever = extractUrlParameter("opdrachtgever");
-    const opdrachtgeverid = extractUrlParameter("opdrachtgeverid");
-    const naamklant = extractUrlParameter("naamklant");
-    const adres = extractUrlParameter("adres");
-    const soortklus = extractUrlParameter("soortklus");
-    const formname = extractUrlParameter("formname");
-    
-    // Split dlform and dlsign
-    const dlformSplit = url.split("dlform="); // splitst het eerste laatste deel van de url vanaf "dlform="
-    const afterDlform = dlformSplit[dlformSplit.length - 1]; // pakt het gedeelte van de url
-    const dlArray = afterDlform.split("&dlsign=");
+    // Array met vereiste parameters
+    const requiredParameters = ["taskid", "formid", "opdref", "opdrachtgever", "opdrachtgeverid", "naamklant", "adres", "soortklus", "formname", "dlform"];
 
-    const dlform = dlArray[0];
-    const dlsign = dlArray[1];
-    
-    // initialize trigger url
-    const flowTriggerUrl = "https://prod-159.westeurope.logic.azure.com:443/workflows/8b2be737ee804dbc902b49adde2b1a5e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IvKTz0kCTEdvFW9vExUQHKnWYWVl-Qe_eTTWV4C_L08";
+    // Voer de code uit op basis van de aanwezigheid van parameters
+    try {
+        if (requiredParameters.every(controleerParameter)) {
+            runMainJs();
+        } else {
+            runErrorScreen("Niet alle vereiste parameters zijn aanwezig in de URL.");
+        }
+    } catch (error) {
+        runErrorScreen("Er is een fout opgetreden bij het controleren van de parameters.");
+    }
 
-    // trigger power automate to process the workorder completion
-    fetch(flowTriggerUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            {
-                "taskid": taskid,
-                "formid": formid,
-                "opdref": opdref,
-                "opdrachtgever": opdrachtgever,
-                "opdrachtgeverid": opdrachtgeverid,
-                "naamklant": naamklant,
-                "adres": adres,
-                "soortklus": soortklus,
-                "formname": formname,
-                "dlform": dlform,
-                "dlsign": dlsign
-            }
-        )
-    })
-        .then(response => {
-            // Verwerk de response
-            console.log('Flow trigger response:', response);
+    function runErrorScreen(foutmelding) {
+        // Toon het error_screen en verberg andere schermen
+        document.getElementById("error_screen").style.display = "flex";
+        document.getElementById("success_screen").style.display = "none";
+        document.getElementById("loading_screen").style.display = "none";
+
+        // Stel de foutmeldingstekst in
+        document.getElementById("error_message_box").textContent = foutmelding;
+    }
+
+    function runSuccessCreen() {
+        // Toon het error_screen en verberg andere schermen
+        document.getElementById("error_screen").style.display = "none";
+        document.getElementById("success_screen").style.display = "flex";
+        document.getElementById("loading_screen").style.display = "none";
+    }
+
+    // Functie om te controleren of een parameter aanwezig is in de URL
+    function controleerParameter(parameterNaam) {
+        const urlParameters = new URLSearchParams(url);
+        return urlParameters.has(parameterNaam);
+    }
+
+    // Functie om een URL-parameter te extraheren
+    function extractUrlParameter(sleutel) {
+        const urlObj = new URL(url);
+        const params = new URLSearchParams(urlObj.search);
+        return params.get(sleutel);
+    }
+
+    // Start de codeverwerkingsfunctie
+    function runMainJs() {
+        // Extract de parameters met behulp van de extractUrlParameter functie
+        const taskid = extractUrlParameter("taskid");
+        const formid = extractUrlParameter("formid");
+        const opdref = extractUrlParameter("opdref");
+        const opdrachtgever = extractUrlParameter("opdrachtgever");
+        const opdrachtgeverid = extractUrlParameter("opdrachtgeverid");
+        const naamklant = extractUrlParameter("naamklant");
+        const adres = extractUrlParameter("adres");
+        const soortklus = extractUrlParameter("soortklus");
+        const formname = extractUrlParameter("formname");
+
+        // Split dlform en dlsign
+        const dlformSplit = url.split("dlform=");
+        const naDlform = dlformSplit[dlformSplit.length - 1];
+        const dlArray = naDlform.split("&dlsign=");
+
+        const dlform = dlArray[0];
+        let dlsign = "";
+
+        // Als dlsign bestaat, stel dan de parameterwaarde in
+        if (dlArray.length > 1) {
+            dlsign = dlArray[1];
+        };
+
+        // Initialiseer de trigger-URL
+        const flowTriggerUrl = "https://prod-159.westeurope.logic.azure.com:443/workflows/8b2be737ee804dbc902b49adde2b1a5e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IvKTz0kCTEdvFW9vExUQHKnWYWVl-Qe_eTTWV4C_L08";
+
+        // Activeer Power Automate om de voltooiing van de werkorder te verwerken
+        fetch(flowTriggerUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    "taskid": taskid,
+                    "formid": formid,
+                    "opdref": opdref,
+                    "opdrachtgever": opdrachtgever,
+                    "opdrachtgeverid": opdrachtgeverid,
+                    "naamklant": naamklant,
+                    "adres": adres,
+                    "soortklus": soortklus,
+                    "formname": formname,
+                    "dlform": dlform,
+                    "dlsign": dlsign
+                }
+            )
         })
-        .catch(error => {
-            // Verwerk de error
-            console.error('Flow trigger error:', error);
-        });
+            .then(response => {
+                // Verwerk de respons
+                console.log('Flow trigger respons:', response);
+                runSuccessCreen();
+            })
+            .catch(error => {
+                // Verwerk de fout
+                console.error('Flow trigger fout:', error);
+                runErrorScreen(error);
+            });
 
-}
+    }
 
-// Voer de code uit op basis van de aanwezigheid van parameters
-if (requiredParameters.every(controleerParameter)) {
-    runMainJs();
-} else {
-    runErrorScreen();
-}
-
-function runErrorScreen() {
-    // Code voor het tonen van foutmeldingen of iets dergelijks
-}
+});
