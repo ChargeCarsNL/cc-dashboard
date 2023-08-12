9b686350cb4d629a24fdb9e73e7a66e2007854e9
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-12 23:03:47 +0200

    changes

--- customJsConfirmationPage.js
+++ customJsConfirmationPage.js
@@ -7,18 +7,30 @@ window.addEventListener('load', function () {
     // Array met vereiste parameters
     const requiredParameters = ["taskid", "formid", "opdref", "opdrachtgever", "opdrachtgeverid", "naamklant", "adres", "soortklus", "formname", "dlform"];
 
-    // Voer de code uit op basis van de aanwezigheid van parameters
-    try {
-        if (requiredParameters.every(controleerParameter)) {
-            runMainJs();
-        } else {
-            runErrorScreen("Niet alle vereiste parameters zijn aanwezig in de URL.");
+    // Functie om te controleren of een parameter aanwezig is in de URL en een string is
+    function controleerParameters() {
+        const urlParameters = new URLSearchParams(window.location.search);
+
+        for (const parameterNaam of requiredParameters) {
+            const parameterWaarde = urlParameters.get(parameterNaam);
+            if (typeof parameterWaarde !== 'string' || parameterWaarde === '') {
+                console.error(`Vereiste parameter ontbreekt of heeft geen geldige waarde: ${parameterNaam}`);
+                return false;
+            }
         }
-    } catch (error) {
-        runErrorScreen("Er is een fout opgetreden bij het controleren van de parameters.");
+
+        return true;
+    }
+
+    // Voer de code uit op basis van de aanwezigheid van parameters
+    if (controleerParameters()) {
+        runMainJs();
+    } else {
+        runErrorScreen("Niet alle vereiste parameters zijn aanwezig in de URL.");
     }
 
     function runErrorScreen(foutmelding) {
+
         // Toon het error_screen en verberg andere schermen
         document.getElementById("error_screen").style.display = "flex";
         document.getElementById("success_screen").style.display = "none";
@@ -78,38 +90,46 @@ window.addEventListener('load', function () {
         const flowTriggerUrl = "https://prod-159.westeurope.logic.azure.com:443/workflows/8b2be737ee804dbc902b49adde2b1a5e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IvKTz0kCTEdvFW9vExUQHKnWYWVl-Qe_eTTWV4C_L08";
 
         // Activeer Power Automate om de voltooiing van de werkorder te verwerken
+        const jsonBody = {
+            "taskid": taskid,
+            "formid": formid,
+            "opdref": opdref,
+            "opdrachtgever": opdrachtgever,
+            "opdrachtgeverid": opdrachtgeverid,
+            "naamklant": naamklant,
+            "adres": adres,
+            "soortklus": soortklus,
+            "formname": formname,
+            "dlform": dlform,
+            "dlsign": dlsign
+        }
+
+        console.log(jsonBody);
+
         fetch(flowTriggerUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(
-                {
-                    "taskid": taskid,
-                    "formid": formid,
-                    "opdref": opdref,
-                    "opdrachtgever": opdrachtgever,
-                    "opdrachtgeverid": opdrachtgeverid,
-                    "naamklant": naamklant,
-                    "adres": adres,
-                    "soortklus": soortklus,
-                    "formname": formname,
-                    "dlform": dlform,
-                    "dlsign": dlsign
-                }
+                jsonBody
             )
         })
             .then(response => {
-                // Verwerk de respons
-                console.log('Flow trigger respons:', response);
+                return response.json();
+            })
+            .then(data => {
+                console.log('Flow trigger respons:', data);
                 runSuccessCreen();
             })
             .catch(error => {
-                // Verwerk de fout
                 console.error('Flow trigger fout:', error);
+
+                // Log de volledige foutrespons
+                console.log('Volledige foutrespons:', error.response);
+
                 runErrorScreen(error);
             });
-
     }
 
 });
