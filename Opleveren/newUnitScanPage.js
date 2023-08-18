4462fb186b06bf802fa5efa53b027e0496862bfc
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-19 00:55:18 +0200

    c

--- Opleveren/newUnitScanPage.js
+++ Opleveren/newUnitScanPage.js
@@ -3,6 +3,7 @@
 let newUnitScreen = document.getElementById('new_unit_screen');
 newUnitScreen.style.display = 'none';
 
+
 let errorMessageScreen = document.getElementById('error_message_screen');
 let errorMessageText = document.getElementById('error_message_text');
 let succesMessageScreen = document.getElementById('succes_message_screen');
@@ -49,47 +50,59 @@ window.addEventListener('load', function () {
     }
 });
 
-// -------------------On barcode change-----------------------
+// -------------------event listeners-----------------------
 
 // Voeg een event listener toe voor het 'input' event
 barcodeInputElement.addEventListener('input', async function () {
+
     const scannedCode = barcodeInputElement.value;
-    barcodeInputElement.value = null;
+    console.log(`Barcode scanned: ${scannedCode}`);
 
-    // Krijg de index van het het voorraad select input veld
-    const selectedIndex = voorraadSelect.selectedIndex;
-    console.log(`Selected index: ${selectedIndex}`);
+    runLoadingScreen();
 
-    if (scannedCode.length > 0) {
-        if (selectedIndex > 0) {
-            currentUnitClassItem = await getUnitClass(scannedCode);
+    if (checkEANValidity(scannedCode)) {
+        // Krijg de index van het het voorraad select input veld
+        const selectedIndex = voorraadSelect.selectedIndex;
+        console.log(`Controleren of unit bekend is...`);
+        if (scannedCode.length > 0) {
+            if (selectedIndex > 0) {
 
-            if (currentUnitClassItem != null) {
-                addUnitToSelectedVoorraad(scannedCode, currentUnitClassItem, selectedIndex);
+                currentUnitClassItem = await getUnitClass(scannedCode);
+
+                if (currentUnitClassItem != null) {
+                    addUnitToSelectedVoorraad(scannedCode, currentUnitClassItem, selectedIndex);
+                } else {
+                    stopLoadingScreen();
+                    runNewUnitScreen();
+                }
             } else {
-                runNewUnitScreen();
+                stopLoadingScreen();
+                console.log("Geannulleerd. Geen voorraad geselecteerd");
+                runErrorMessage('Geen voorraad geselecteerd');
             }
-        } else {
-            console.log("ran error screen");
-            runErrorMessage('Geen voorraad geselecteerd');
         }
+    } else {
+        runErrorMessage(`EAN code: ${scannedCode} is ongeldig`);
     }
 });
 
 unitToevoegenButton.addEventListener("click", function () {
+    runLoadingScreen();
     // add filled unit to clickup list
 
     const scannedCode = barcodeInputElement.value;
     const filledValue = newUnitTextInput.value;
     if (filledValue.length > 0) {
+        stopNewUnitScreen();
         addItemToUnitClassList(scannedCode, filledValue)
     } else {
+        stopLoadingScreen();
         console.error('Error:', 'Geen waarde opgegeven');
     }
 });
 
 closeButtonNewUnitScreen.addEventListener("click", function () {
-    newUnitScreen.style.display = "none";
+    stopNewUnitScreen();
     console.log('closed');
 });
 
@@ -103,6 +116,7 @@ async function getUnitClass(scannedCode) {
         const data = await response.json();
         const unitClassItemsArray = data.tasks;
         const EANCodeCustomFieldId = '7bb86afc-301a-48ec-b618-a71b2afb6ebd';
+        console.log(`Current scannedCode: ${scannedCode}`);
 
         for (let i = 0; i < unitClassItemsArray.length; i++) {
             let customFieldsArray = unitClassItemsArray[i].custom_fields;
@@ -146,9 +160,15 @@ function addItemToUnitClassList(unitCode, unitName) {
     })
         .then(response => response.json())
         .then(data => {
+            barcodeInputElement.value = '';
+            stopLoadingScreen();
+            runSuccesMessage(`${unitName} toegevoegd aan bekende units`);
             console.log(`Unit met id:${data.created_item_id} toegevoegd aan lijst`);
         })
         .catch(error => {
+            barcodeInputElement.value = '';
+            stopLoadingScreen();
+            runErrorMessage(`Kon ${unitName} niet toevoegen aan bekende units`);
             console.error('Error:', error);
         });
 }
@@ -162,9 +182,11 @@ function addUnitToSelectedVoorraad(scannedCode, currentUnitClassItem, selectedIn
         "value": selectedOption.value
     }
 
+    console.log(`Unit is bekend: ${currentUnitClassItem.name}. Unit wordt toegevoegd aan ${selectedVoorraad.name}...`);
+
     const unitClassName = currentUnitClassItem.name;
     const unitClassId = currentUnitClassItem.id;
-    
+
     // voeg item toe aan juiste clickup voorraad lijst
     const url = 'https://prod-189.westeurope.logic.azure.com:443/workflows/db5e25179b4b4ac8bb43551fb17388ba/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=e68NLwrVMujBi3KiJ7SvEkGUMWDAL8Vukh8Qx88QD38';
     console.log(`item: ${unitClassName} - ${scannedCode} wordt aan voorraad lijst toegevoegd`)
@@ -182,22 +204,70 @@ function addUnitToSelectedVoorraad(scannedCode, currentUnitClassItem, selectedIn
     })
         .then(response => response.json())
         .then(data => {
-            console.log(`Unit met id:${data.created_item_id} toegevoegd aan voorraad`);
+            console.log(`Unit met id:${data.created_item_id} succesvol toegevoegd aan voorraad`);
+            barcodeInputElement.value = '';
+            stopLoadingScreen();
             runSuccesMessage(`${unitClassName} succesvol toegevoegd aan ${selectedVoorraad.name}`);
         })
         .catch(error => {
             console.error('Error:', error);
+            barcodeInputElement.value = '';
+            stopLoadingScreen();
             runErrorMessage(`${unitClassName} kon niet worden toegevoegd aan ${selectedVoorraad.name}`);
         });
 }
 
-function runNewUnitScreen() {
-    newUnitScreen.style.display = 'flex';
+// Controleer EAN code op geldigheid
+function checkEANValidity(code) {
+    // Verwijder spaties en andere tekens die geen cijfers zijn
+    code = code.replace(/\D/g, '');
+
+    // Controleer of de code 8 of 13 cijfers heeft
+    if (code.length !== 8 && code.length !== 13) {
+        console.log("Ongeldige lengte: EAN-codes moeten 8 of 13 cijfers lang zijn.");
+        return false;
+    }
+
+    if (code.length === 8) {
+        // EAN-8 controlecijfer berekening
+        let sum = 0;
+        for (let i = 0; i < code.length - 1; i++) {
+            let digit = parseInt(code[i]);
+            sum += i % 2 === 0 ? digit * 3 : digit;
+        }
+        let checksum = (10 - (sum % 10)) % 10;
+
+        // Controleer of het controlecijfer overeenkomt met het laatste cijfer in de code
+        if (checksum !== parseInt(code[code.length - 1])) {
+            console.log("Ongeldig controlecijfer voor EAN-8 code.");
+            return false;
+        }
+    } else if (code.length === 13) {
+        // EAN-13 controlecijfer berekening
+        let sum = 0;
+        for (let i = 0; i < code.length - 1; i++) {
+            let digit = parseInt(code[i]);
+            sum += i % 2 === 0 ? digit : digit * 3;
+        }
+        let checksum = (10 - (sum % 10)) % 10;
+
+        // Controleer of het controlecijfer overeenkomt met het laatste cijfer in de code
+        if (checksum !== parseInt(code[code.length - 1])) {
+            console.log("Ongeldig controlecijfer voor EAN-13 code.");
+            return false;
+        }
+    }
+
+    console.log('EAN-code is gecontroleerd en is geldig');
+    return true;
 }
 
 function runErrorMessage(message) {
+    stopLoadingScreen();
+    console.error(`Fout. ${message}`);
     errorMessageText.textContent = message;
     errorMessageScreen.style.display = 'flex'; // Display the element
+    barcodeInputElement.value = '';
 
     // After the specified duration, hide the element
     setTimeout(function () {
@@ -206,11 +276,36 @@ function runErrorMessage(message) {
 }
 
 function runSuccesMessage(message) {
+    stopLoadingScreen();
+    console.log(`Succes. ${message}`);
     succesMessageText.textContent = message;
     succesMessageScreen.style.display = 'flex'; // Display the element
+    barcodeInputElement.value = '';
 
     // After the specified duration, hide the element
     setTimeout(function () {
         succesMessageScreen.style.display = 'none'; // Hide the element
     }, 3000);
+}
+
+function runNewUnitScreen() {
+    console.log(`Unit is onbekend. Voeg hem eventueel toe aan bekende units.`);
+    newUnitScreen.style.display = 'flex';
+}
+
+function stopNewUnitScreen() {
+    newUnitScreen.style.display = 'none';
+    newUnitTextInput.value = '';
+    barcodeInputElement.value = '';
+}
+
+function runLoadingScreen() {
+    console.log(`Aan het laden...`);
+    const loadingScreen = document.getElementById('loading_screen');
+    loadingScreen.style.display = 'flex';
+}
+
+function stopLoadingScreen() {
+    const loadingScreen = document.getElementById('loading_screen');
+    loadingScreen.style.display = 'none';
 }
