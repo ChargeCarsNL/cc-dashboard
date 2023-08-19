f0dbdebdb6167a1e763076001ad4a152025b1bb6
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-19 23:25:06 +0200

    Update proxyFetchPowerAutomate.js

--- Opleveren/proxyFetchPowerAutomate.js
+++ Opleveren/proxyFetchPowerAutomate.js
@@ -4,11 +4,18 @@ function proxyFetch(url, requestObj) {
 
     proxyUrl = 'https://prod-111.westeurope.logic.azure.com:443/workflows/c1a7a2f208da4ca89aaca0346bdc3e98/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8w8AVtZo0uZQrpfVGOvYgcF6_UFDnbUSrns_ywHowos';
     requestObj.headers.method = method
+    requestObj.headers.url = url;
+
+    const headers = requestObj.headers;
+    const body = requestObj.body;
+
+    console.log(`Running request with method: ${method}, Headers: ${headers}, Body: ${body}`);
+    console.log(headers);
 
     fetch(proxyUrl, {
         method: 'POST',
-        headers: requestObj.headers,
-        body: requestObj.body
+        headers: headers,
+        body: body
     })
         .then(response => {
             return response.json();
