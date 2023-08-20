8a6df9b61e785ee5e2d1621a6fbca3c26014a96f
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-20 19:28:04 +0000

    v

---
+++ javascript/opleveren/autofill select options form.js
@@ -0,0 +1,9 @@
+function autoFillLaadpalen() {
+
+    const keyword = 'repeater_field_3'; // the keyword the elements must contain
+
+    // Use querySelectorAll to select elements with IDs containing the keyword
+    const elementsWithKeyword = document.querySelectorAll(`[name*="${keyword}"]`);
+
+    
+}
