595c9bb7c39fa586e4579dcec86769e47dc74977
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-20 23:54:17 +0200

    c

--- javascript/globalfunctions/proxyFetchPowerAutomate.js
+++ javascript/globalfunctions/proxyFetchPowerAutomate.js
@@ -1,15 +1,21 @@
 async function proxyFetch(url, requestObj) {
-
     const method = requestObj.method;
 
-    proxyUrl = 'https://prod-111.westeurope.logic.azure.com:443/workflows/c1a7a2f208da4ca89aaca0346bdc3e98/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8w8AVtZo0uZQrpfVGOvYgcF6_UFDnbUSrns_ywHowos';
-    requestObj.headers.method = method
+    const proxyUrl = 'https://prod-111.westeurope.logic.azure.com:443/workflows/c1a7a2f208da4ca89aaca0346bdc3e98/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8w8AVtZo0uZQrpfVGOvYgcF6_UFDnbUSrns_ywHowos';
+    
+    if (requestObj.headers && requestObj.headers.Authorization) {
+        // Rename the property from Authorization to _Authorization
+        requestObj.headers._Authorization = requestObj.headers.Authorization;
+        delete requestObj.headers.Authorization;
+    }
+
+    requestObj.headers.method = method;
     requestObj.headers.url = url;
 
     const headers = requestObj.headers;
     const body = requestObj.body;
 
-    console.log(`Running request with method: ${method}, Headers: ${headers}, Body: ${body}`);
+    console.log(`Running request with method: ${method}, Headers: ${JSON.stringify(headers)}, Body: ${body}`);
 
     try {
         const response = await fetch(proxyUrl, requestObj);
--- javascript/opleveren/autofill select options form.js
+++ javascript/opleveren/autofill select options form.js
@@ -1,9 +1,63 @@
-function autoFillLaadpalen() {
+async function autoFillLaadpalen() {
+    const keyword = 'repeater_field_3';
+    const selectElementsWithKeyword = document.querySelectorAll(`[name*="${keyword}"]`);
 
-    const keyword = 'repeater_field_3'; // the keyword the elements must contain
+    const laadpaalSoortenObj = await getLaadpaalSoorten().tasks;
 
-    // Use querySelectorAll to select elements with IDs containing the keyword
-    const elementsWithKeyword = document.querySelectorAll(`[name*="${keyword}"]`);
+    if (laadpaalSoortenObj && laadpaalSoortenObj.length > 0) {
+        const options = laadpaalSoortenObj.map(soort => {
+            return {
+                'name': soort.name,
+                'value': soort.id
+            };
+        });
 
-    
+        selectElementsWithKeyword.forEach(selectElement => {
+            options.forEach(option => {
+                if (!selectElement.querySelector(`option[value="${option.value}"]`)) {
+                    const newOption = document.createElement('option');
+                    newOption.value = option.value;
+                    newOption.textContent = option.name;
+                    selectElement.appendChild(newOption);
+                }
+            });
+        });
+    }
 }
+
+async function getLaadpaalSoorten() {
+
+    const unitClassItemsList = '900401619864';
+    const customFieldQuery = JSON.stringify([
+        {
+            "field_id": "da66ae38-b086-437d-bba2-92cb9619c07c",
+            "operator": "=",
+            "value": "0"
+        }
+    ]);
+
+    const apiKey = 'pk_38199608_RWBKIFPYH9LSP1DAR6ZLNJD2ZQP5IY4T';
+    const url = `https://api.clickup.com/api/v2/list/${unitClassItemsList}/task?custom_fields=${customFieldQuery}`;
+
+    return await proxyFetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: apiKey
+        }
+    });
+
+}
+
+const repeatButtons = document.getElementsByClassName('repeat-plus');
+
+// run autofill laadpaal options when extra laadpaal is added
+Array.from(repeatButtons).forEach(button => {
+    button.addEventListener('click', function () {
+        autoFillLaadpalen();
+    });
+});
+
+// run autofill laadpaal options when page is loaded
+window.addEventListener('load', function () {
+    autoFillLaadpalen();
+});
