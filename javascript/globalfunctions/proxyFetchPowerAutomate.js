d03324e2c1f696d03a6311c695f78dafd8e8e9f1
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-21 19:02:39 +0200

    c

--- javascript/globalfunctions/proxyFetchPowerAutomate.js
+++ javascript/globalfunctions/proxyFetchPowerAutomate.js
@@ -2,7 +2,7 @@ async function proxyFetch(url, requestObj) {
     const method = requestObj.method;
 
     const proxyUrl = 'https://prod-111.westeurope.logic.azure.com:443/workflows/c1a7a2f208da4ca89aaca0346bdc3e98/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=8w8AVtZo0uZQrpfVGOvYgcF6_UFDnbUSrns_ywHowos';
-    
+
     if (requestObj.headers && requestObj.headers.Authorization) {
         // Rename the property from Authorization to _Authorization
         requestObj.headers._Authorization = requestObj.headers.Authorization;
@@ -21,10 +21,13 @@ async function proxyFetch(url, requestObj) {
 
     try {
         const response = await fetch(proxyUrl, requestObj);
+        console.log('Raw response:', response); // Voeg deze log toe
         if (!response.ok) {
             throw new Error(`Fetch failed with status ${response.status}`);
         }
-        return response.json();
+        const jsonResponse = await response.json();
+        console.log('Parsed response:', jsonResponse); // Voeg deze log toe
+        return jsonResponse;
     } catch (error) {
         console.error('Proxy fetch error:', error);
         throw error;
