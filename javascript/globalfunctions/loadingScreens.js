59cd77d996ddf03978c68833e49f348ff2d30ebd
ChargeCarsNL <141914103+ChargeCarsNL@users.noreply.github.com>
2023-08-24 10:35:38 +0200

    c

--- javascript/globalfunctions/loadingScreens.js
+++ javascript/globalfunctions/loadingScreens.js
@@ -1,16 +1,34 @@
 function runLoadingScreen(message) {
+
+    const brixContent = document.getElementById('brx-content');
+
+    if (document.getElementById('brx-content')) {
+
+        brixContent.style.display = 'none';
+
+    }
+
     console.log(`Aan het laden...`);
     const loadingScreen = document.getElementById('loading_screen');
     loadingScreen.style.display = 'flex';
-    if (message && message.length > 0 ) {
+    if (message && message.length > 0) {
         document.getElementById('loading_text').innerHTML = message;
     } else {
         document.getElementById('loading_text').innerHTML = 'Aan het laden...';
     }
 }
-    
+
 
 function stopLoadingScreen() {
+
+    const brixContent = document.getElementById('brx-content');
+
+    if (document.getElementById('brx-content')) {
+
+        brixContent.style.display = 'block';
+
+    }
+
     const loadingScreen = document.getElementById('loading_screen');
     loadingScreen.style.display = 'none';
     document.getElementById('loading_text').innerHTML = '';
